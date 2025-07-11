4a716a79818b3d62c486628eeed762e6354f5455
Myroslav13 <myroslavshchuruk@gmail.com>
2025-07-11 19:15:30 +0300

    desktop version done

--- myScript.js
+++ myScript.js
@@ -1,3 +1,4 @@
+// Site elements
 const inputMortgageAmount = document.getElementById('input-mortgage-amount')
 const containerMortgageAmount = document.getElementById('container-mortgage-amount')
 
@@ -5,7 +6,7 @@ const inputMortgageTerm = document.getElementById('input-mortgage-term')
 const containerMortgageTerm = document.getElementById('container-mortgage-term')
 
 const inputInterestRate = document.getElementById('input-interest-rate')
-const containerMortgageRate = document.getElementById('container-interest-rate')
+const containerInterestRate = document.getElementById('container-interest-rate')
 
 const radioContainer1 = document.getElementById('radio-container-1')
 const radioContainer2 = document.getElementById('radio-container-2')
@@ -15,14 +16,22 @@ const radioInterestOnly = document.getElementById('radio-interest-only')
 const buttonSubmit = document.getElementById('button-submit')
 
 const resultsSide = document.getElementById('results-side')
+const warning1 = document.getElementById('warning-1')
+const warning2 = document.getElementById('warning-2')
+const warning3 = document.getElementById('warning-3')
+const warning4 = document.getElementById('warning-4')
+
+const clearAll = document.getElementById('clear-all')
 
 function inputFocus(input, container) {
     input.style.border = "1px solid #dbda32"
+    container.classList.remove('warning-container')
     container.classList.add('focused')
 }
 
 function inputBlur(input, container) {
     input.style.border = "1px solid #838d8f"
+    container.classList.remove('warning-container')
     container.classList.remove('focused')
 }
 
@@ -38,42 +47,137 @@ function radioBlur(radio, container) {
     container.style.backgroundColor = "white"
 }
 
+// Calculation button clicked
 function buttonClicked(event) {
     event.preventDefault();
 
-    if (inputMortgageAmount.value.trim() === "" || inputInterestRate.value.trim() === "" || inputMortgageTerm.value.trim() === "" || (radioRepayment.checked === false && radioInterestOnly.checked === false)) {
-        return;
+    warning1.style.display = "none"
+    warning2.style.display = "none"
+    warning3.style.display = "none"
+    warning4.style.display = "none"
+
+    let isBlank = false
+
+    if (inputMortgageAmount.value.trim() === "") {
+        isBlank = true
+        warning1.style.display = "block"
+        containerMortgageAmount.classList.add('warning-container')
     }
 
-    resultsSide.innerHTML = `
-    <div class="results-side">
-        <h2>Your results</h2>
+    if (inputMortgageTerm.value.trim() === "") {
+        isBlank = true
+        warning2.style.display = "block"
+        containerMortgageTerm.classList.add('warning-container')
+    }
 
-        <p>Your results are shown below based on the information you provided. To adjust the results, edit the form and click “calculate repayments” again.</p>
-        
-        <div>
-            <h3>Your monthly repayments</h3>
-            <hr align="center">
-            <h3>Total you'll repay over the term</h3>
+    if (inputInterestRate.value.trim() === "") {
+        isBlank = true
+        warning3.style.display = "block"
+        containerInterestRate.classList.add('warning-container')
+    }
+
+    // If radiobuttons aren't active
+    if (radioRepayment.checked === false && radioInterestOnly.checked === false) {
+        isBlank = true
+        warning4.style.display = "block"
+    }
+
+    // If every field is filled
+    if(!isBlank){
+        let p = parseFloat(inputMortgageAmount.value)
+        let r = parseFloat(inputInterestRate.value) / (12 * 100)
+        let n = parseInt(inputMortgageTerm.value) * 12
+
+        let monthlyPayment = ((p * r * Math.pow(1 + r, n)) / (Math.pow(1 + r, n) - 1)).toFixed(2)
+        let totalPayment = (monthlyPayment * n).toFixed(2)
+
+        resultsSide.innerHTML = `
+        <div class="mortgage-results-header">
+            <h2>Your results</h2>
+
+            <p>Your results are shown below based on the information you provided. To adjust the results, edit the form and click “calculate repayments” again.</p>
+            <br>
         </div>
-    </div>`
+
+        <div class="mortgage-results">
+            <h2>Your monthly repayments</h2>
+            <h3 class="mortgage-price">£${monthlyPayment}</h3>
+
+            <hr align="center" border="1px solid gray">
+            
+            <h2>Total you'll repay over the term</h2>
+            <h3 class="mortgage-monthly">£${totalPayment}</h3>
+        </div>`
+
+        resultsSide.style.justifyContent = "start"
+        resultsSide.style.padding = "40px 40px"
+    }
+}
+
+function containerHover(input, container) {
+    input.style.borderColor = "#133040"
+    container.classList.add('hover-effect')
+}
+
+function containerUnhover(container) {
+    container.classList.remove('hover-effect')
+}
+
+// Clear All clicked
+function clearAllClick() {
+    warning1.style.display = "none"
+    warning2.style.display = "none"
+    warning3.style.display = "none"
+    warning4.style.display = "none"
+
+    inputMortgageAmount.value = ""
+    inputMortgageTerm.value = ""
+    inputInterestRate.value = ""
+    radioRepayment.checked = false
+    radioInterestOnly.checked = false
+
+    inputBlur(inputMortgageAmount, containerMortgageAmount)
+    inputBlur(inputMortgageTerm, containerMortgageTerm)
+    inputBlur(inputInterestRate, containerInterestRate)
+    radioBlur(radioRepayment, radioContainer1)
+    radioBlur(radioInterestOnly, radioContainer2)
+
+    resultsSide.innerHTML = `
+        <img src="assets/images/illustration-empty.svg" alt="illustration-empty" draggable="false" id="illustration-empty">
+        <h2 align="center">Results shown here</h2>
+
+        <p align="center">Complete the form and click “calculate repayments” to see what 
+        your monthly repayments would be.</p>`
+
+    resultsSide.style.justifyContent = "center"
+    resultsSide.style.padding = "0px 40px"
 }
 
 // Listeners
-inputMortgageAmount.addEventListener('focus', () => inputFocus(inputMortgageAmount, containerMortgageAmount));
-inputMortgageAmount.addEventListener('blur', () => inputBlur(inputMortgageAmount, containerMortgageAmount));
+inputMortgageAmount.addEventListener('focus', () => inputFocus(inputMortgageAmount, containerMortgageAmount))
+inputMortgageAmount.addEventListener('blur', () => inputBlur(inputMortgageAmount, containerMortgageAmount))
 
-inputMortgageTerm.addEventListener('focus', () => inputFocus(inputMortgageTerm, containerMortgageTerm));
-inputMortgageTerm.addEventListener('blur', () => inputBlur(inputMortgageTerm, containerMortgageTerm));
+inputMortgageTerm.addEventListener('focus', () => inputFocus(inputMortgageTerm, containerMortgageTerm))
+inputMortgageTerm.addEventListener('blur', () => inputBlur(inputMortgageTerm, containerMortgageTerm))
 
-inputInterestRate.addEventListener('focus', () => inputFocus(inputInterestRate, containerMortgageRate));
-inputInterestRate.addEventListener('blur', () => inputBlur(inputInterestRate, containerMortgageRate));
+inputInterestRate.addEventListener('focus', () => inputFocus(inputInterestRate, containerInterestRate))
+inputInterestRate.addEventListener('blur', () => inputBlur(inputInterestRate, containerInterestRate))
 
-radioRepayment.addEventListener('focus', () => radioFocus(radioRepayment, radioContainer1));
-radioRepayment.addEventListener('blur', () => radioBlur(radioRepayment, radioContainer1));
+radioRepayment.addEventListener('focus', () => radioFocus(radioRepayment, radioContainer1))
+radioRepayment.addEventListener('blur', () => radioBlur(radioRepayment, radioContainer1))
 
-radioInterestOnly.addEventListener('focus', () => radioFocus(radioInterestOnly, radioContainer2));
-radioInterestOnly.addEventListener('blur', () => radioBlur(radioInterestOnly, radioContainer2));
+radioInterestOnly.addEventListener('focus', () => radioFocus(radioInterestOnly, radioContainer2))
+radioInterestOnly.addEventListener('blur', () => radioBlur(radioInterestOnly, radioContainer2))
 
 buttonSubmit.addEventListener('click', buttonClicked)
 
+containerMortgageAmount.addEventListener('mouseenter', () => containerHover(inputMortgageAmount, containerMortgageAmount))
+containerMortgageAmount.addEventListener('mouseleave', () => containerUnhover(containerMortgageAmount))
+
+containerMortgageTerm.addEventListener('mouseenter', () => containerHover(inputMortgageTerm, containerMortgageTerm))
+containerMortgageTerm.addEventListener('mouseleave', () => containerUnhover(containerMortgageTerm))
+
+containerInterestRate.addEventListener('mouseenter', () => containerHover(inputInterestRate, containerInterestRate))
+containerInterestRate.addEventListener('mouseleave', () => containerUnhover(containerInterestRate))
+
+clearAll.addEventListener('click', clearAllClick)
